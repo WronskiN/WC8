de722d25805cee36e9e139ae96a87860d7565996
WronskiN <norbertwronski@yahoo.com>
2019-11-15 10:27:06 +0000

    Section _brands _team _portfolio _footer
    Complete styles inlcuding hover styles.
    
    Javascript
    
    Make navigation menu hide after click on link.
    Set smooth scroll to the section after click on link
    (you can use Jquery).

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 ///////////////////////////////////
 // NAV menu - hamburger for small screen/mobile
-const mobileNav = () => {
-const btnHamburger = document.querySelector('.nav__btn');
 const nav = document.querySelector('.nav__list');
+const btnHamburger = document.querySelector('.nav__btn');
+const mobileNav = () => {
 
 btnHamburger.addEventListener("click", () => {
   btnHamburger.classList.toggle('btn--active');
@@ -10,3 +10,28 @@ btnHamburger.addEventListener("click", () => {
 });
 }
 mobileNav();
+
+//////////////////////////////////////
+/// Nav menu - Hide menu after click on link "a"
+const items = [...document.querySelectorAll('.nav__list--link')];
+items.forEach(function(e) {
+  e.addEventListener('click', () => {
+    nav.classList.remove('nav__list__mobile');
+    btnHamburger.classList.toggle('btn--active');
+  }, 1000);
+});
+
+
+///////////////////////////////////////////////////////////////////////
+//////////////// JQuery Smooth Scroll (Nav)
+$('.nav__list--link').on('click', function(e) {
+  if(this.hash !== '') {
+    e.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800);
+  }
+});
